8e944b5ef7004e32a30caade8cc8386404ebf815
German Gabriel <germancin@users.noreply.github.com>
2018-02-08 04:49:36 -0500

    Update BtnConnectComp.js

--- src/components/Connections/BtnConnectComp.js
+++ src/components/Connections/BtnConnectComp.js
@@ -3,32 +3,31 @@ import styled  from 'styled-components';
 
 function BtnConnect(props) {
     
-const Btn = styled.div`
-    
-    button {
-        margin-bottom:15px;
-        border: 1px solid #0084bf;
-        color:#0084bf;
-        padding:0px 25px 0px 25px;
-        letter-spacing: 1px;
-        width:${(props.size < 100)? props.size : '100'}%;
-    }
-    
-    button:hover {
-        background-color:#B5D1E3;
-        padding:0px 25px 0px 25px;
-        letter-spacing: 1px;
-        cursor: pointer;
-    }
+    const Btn = styled.div`
 
-`;
+        button {
+            margin-bottom:15px;
+            border: 1px solid #0084bf;
+            color:#0084bf;
+            padding:0px 25px 0px 25px;
+            letter-spacing: 1px;
+            width:${(props.size < 100)? props.size : '100'}%;
+        }
 
-  return (
+        button:hover {
+            background-color:#B5D1E3;
+            padding:0px 25px 0px 25px;
+            letter-spacing: 1px;
+            cursor: pointer;
+        }
+
+    `;
+
+    return (
              <Btn>
                 <button>{props.copy}</button>
              </Btn>
-             
-  );
+    );
 }
 
 export default BtnConnect;
